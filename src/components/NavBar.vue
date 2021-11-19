ee42f7057abe221bab9c60f61662eb3709124547
William Caouette <1841363@cgmatane.qc.ca>
2021-11-19 11:38:19 -0500

    changement

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Accueil</router-link> |
+    <router-link to="/">Jouer</router-link> |
+    <router-link to="/">Entrainement</router-link> 
+  <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="home">
+    <h1>Guess my draw</h1>
   </div>
+  <nav-bar></nav-bar>
 </template>
 
 <script>
-// @ is an alias to /src
+import NavBar from '../components/NavBar.vue'
 
 export default {
+  components: {NavBar },
   name: 'Home',
 }
 </script>
