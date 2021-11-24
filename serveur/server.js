994e6c506ffb1a2f72b0faf7f0a3a1be55284c12
Noa Prevel <suplokier@gmail.com>
2021-11-23 20:09:54 -0500

    Ajout de quick Db + Ajout du Get

--- serveur/server.js
+++ serveur/server.js
@@ -16,9 +16,11 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-//db
+//----------------------------------------
+// Base de données
 
 const shortId = require("shortid");
+const db = require("quick.db");
 
 //----------------------------------------
 // Fichiers "statiques"
@@ -26,6 +28,34 @@ const shortId = require("shortid");
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
+
+//-------------------------------
+// Get
+
+// Récupérer toute la collection //
+app.get("/api/quizz", (req, res) => {
+    res.send(db.get("quizz.list"));
+});
+
+// Récupérer un item de la collection //
+app.get("/api/quizz/:id", (req, res) => {
+    const quizz = db.get("quizz.list").find((c) => c.id == req.params.id);
+
+    if (!quizz) {
+        return res.status(404).send("Aucun quizz ne correspond à cet identifiant");
+    }
+
+    res.status(200).send(quizz);
+});
+
+//-------------------------------
+// Post
+
+//-------------------------------
+// Update
+
+//-------------------------------
+// Delete (optionnel)
 
 //-------------------------------
 // 404
@@ -42,3 +72,11 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Express écoute le port ${port}`);
 });
+
+/* 
+Création de routes : 
+-une pour crée, 
+-une pour toutes les récup, 
+-une pour en récup une seule
+*/
+/*Adapter le code : "express-api-rest-quickdb"*/
